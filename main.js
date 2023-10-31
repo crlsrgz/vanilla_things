6b9822dbce1f99e25ad76803efe5500d3f920cc8
crlsrgz <course@carlosfx.com>
2023-10-31 14:55:39 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+import './index.css'
+import './style.css'
+
+import { setupCounter } from './counter.js'
+
+const text = 'hello'
+
+
+async function getData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+    try {
+      const response = await fetch(apiUrl);
+      let apiQuotes = await response.json();
+
+      // console.log(apiQuotes);
+      
+      return apiQuotes;
+    
+    } catch (error){
+      console.log("error " + error);    
+    }
+  }
+
+const names = await getData();
+
+names.map(element => console.log(element.name));
+
+setupCounter(document.querySelector('#counter'))
